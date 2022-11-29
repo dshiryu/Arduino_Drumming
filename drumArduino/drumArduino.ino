0b091980882175a722cac6b2b0c7759ef0f2715a
ViolettaVenus <116300543+ViolettaVenus@users.noreply.github.com>
2022-11-29 16:55:57 +0100

    Update drumArduino.ino

--- drumArduino/drumArduino.ino
+++ drumArduino/drumArduino.ino
@@ -2,15 +2,22 @@
 #include <SD.h>
 #include <TMRpcm.h> // to play file
 
-//Constants
-#define SD_ChipSelectPin 10//4 
-const int speakerPin = 9;
+// //Constants
+#define SD_ChipSelectPin 4//4 
+const int speakerPin = 8;
 int snareSensor = A0; // indicates where the pin is connected
-int snareRingSensor = A1;
+// int snareRingSensor = A1;
+
+// grün - 4
+// geld nicht SD - 8
+// gelb SD - 11
+// blau - 12
+// orange - 13
+// braun - GRN
 
 // Sensor values
 int snareValue = 0;
-int snareRingValue = 0;
+// int snareRingValue = 0;
 
 // Threshold Reference Values
 int ref1 = 10;
@@ -21,24 +28,22 @@ int ref5 = 600;
 int ref6 = 800;
 
 // Test values for when hits won't surpass 500
-/*
-int ref1 = 10;
-int ref2 = 80;
-int ref3 = 160;
-int ref4 = 240;
-int ref5 = 320;
-int ref6 = 400;
-*/
+// int ref1 = 10;
+// int ref2 = 80;
+// int ref3 = 160;
+// int ref4 = 240;
+// int ref5 = 320;
+// int ref6 = 400;
+
 
 // Test values for when hits won't surpass 250
-/*
-int ref1 = 10;
-int ref2 = 50;
-int ref3 = 85;
-int ref4 = 120;
-int ref5 = 155;
-int ref6 = 190;
-*/
+// int ref1 = 10;
+// int ref2 = 50;
+// int ref3 = 85;
+// int ref4 = 120;
+// int ref5 = 155;
+// int ref6 = 190;
+
 
 // Soundfile names, the lower the number, the lighter the sound
 char* snareSound1 = "snare1.wav"; 
@@ -62,7 +67,7 @@ TMRpcm tmrpcm;
 void setup() {
 	Serial.begin(9600);
 	pinMode(snareSensor, INPUT);
-  pinMode(snareRingSensor, INPUT);
+  // pinMode(snareRingSensor, INPUT);
 	
 	//Initialize SD Shield
 	if (!SD.begin(SD_ChipSelectPin)) {
@@ -72,14 +77,26 @@ void setup() {
 	
 	//Initialize Speaker
 	tmrpcm.speakerPin = speakerPin;
-	tmrpcm.setVolume(3);
+	tmrpcm.setVolume(10);
 }
 
 void loop() {
-	
   // Saves value read from each sensor
-  snareValue = analogRead(snareSensor); 
-  snareRingValue = analogRead(snareRingSensor);
+snareValue = analogRead(snareSensor); 
+// snareRingValue = analogRead(snareRingSensor);
+
+
+
+ if(snareValue > 400){
+      //Serial.println(snareValue + "funktioniert");
+    
+      tone(speakerPin, 131, 10);
+      
+      // delay(1000);
+      // noTone(buzzerPin);
+      // delay(1000);
+      
+    }
 
 	/* 
   The piezo sensor reads values from 0 to 1023, 
@@ -103,30 +120,31 @@ void loop() {
 	} else if (snareValue > ref6){ //ff
 		tmrpcm.play(snareSound6); 
     Serial.println(snareValue);
-	} else if (snareValue > ref6 && snareRingValue > ref3){ // sfx
-		tmrpcm.play(snareSound7); 	
-    Serial.println(snareValue);
-  } 
+	// } else if (snareValue > ref6 && snareRingValue > ref3){ // sfx
+	//  tmrpcm.play(snareSound7); 	
+  //   Serial.println(snareValue);
+  // } 
   
   // Sounds for when the rim alone is hit
-  if (snareRingValue > ref1 && snareRingValue <= ref2){
-		tmrpcm.play(snareRingSound1); 
-    Serial.println(snareRingValue);
-	} else if (snareRingValue > ref2 && snareRingValue <= ref3){
-		tmrpcm.play(snareRingSound2); 
-    Serial.println(snareRingValue);
-	} else if (snareRingValue > ref3 && snareRingValue <= ref4){
-		tmrpcm.play(snareRingSound3); 
-    Serial.println(snareRingValue);
-	} else if (snareRingValue > ref4 && snareRingValue <= ref5){
-		tmrpcm.play(snareRingSound4); 
-    Serial.println(snareRingValue);
-	} else if (snareRingValue > ref5 && snareRingValue <= ref6){
-		tmrpcm.play(snareRingSound5); 
-    Serial.println(snareRingValue);
-	} else if (snareRingValue > ref6){
-		tmrpcm.play(snareRingSound6); 
-    Serial.println(snareRingValue);
-	}
+  // if (snareRingValue > ref1 && snareRingValue <= ref2){
+	// 	tmrpcm.play(snareRingSound1); 
+  //   Serial.println(snareRingValue);
+	// } else if (snareRingValue > ref2 && snareRingValue <= ref3){
+	// 	tmrpcm.play(snareRingSound2); 
+  //   Serial.println(snareRingValue);
+	// } else if (snareRingValue > ref3 && snareRingValue <= ref4){
+	// 	tmrpcm.play(snareRingSound3); 
+  //   Serial.println(snareRingValue);
+	// } else if (snareRingValue > ref4 && snareRingValue <= ref5){
+	// 	tmrpcm.play(snareRingSound4); 
+  //   Serial.println(snareRingValue);
+	// } else if (snareRingValue > ref5 && snareRingValue <= ref6){
+	// 	tmrpcm.play(snareRingSound5); 
+  //   Serial.println(snareRingValue);
+	// } else if (snareRingValue > ref6){
+	// 	tmrpcm.play(snareRingSound6); 
+  //   Serial.println(snareRingValue);
+	// }
 	
-}
+   }
+  }
